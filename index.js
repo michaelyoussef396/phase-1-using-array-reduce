d46fcdfbde2397582d315bf18c10634177dc0ed4
michaelyoussef396 <michaelyoussef396@gmail.com>
2023-11-01 14:25:06 +1100

    finished

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let initalVal = 0;
+const totalBatteries = batteryBatches.reduce(fnRed);
+function fnRed(a, b) {
+  return a + b;
+}
